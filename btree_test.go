d59a14fa15e747765711755fa46c709bfb1e3667
Michael Angerman <stormasm@gmail.com>
2019-03-05 18:07:05 -0500

    shell only --- need to add in an actual test

--- btree_test.go
+++ btree_test.go
@@ -18,11 +18,12 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
-	"reflect"
+//	"os"
+//	"reflect"
 //	"sort"
 //	"strings"
 //	"sync"
-	"testing"
+//	"testing"
 	"time"
 )
 
@@ -74,56 +75,13 @@ func allrev(t *BTree) (out []Item) {
 	return
 }
 
-var btreeDegree = flag.Int("degree", 32, "B-Tree degree")
+var btreeDegree = flag.Int("degree", 3, "B-Tree degree")
 
-func TestBTree(t *testing.T) {
-	tr := New(*btreeDegree, nil)
-	const treeSize = 10000
-	for i := 0; i < 10; i++ {
-		if min := tr.Min(); min != nil {
-			t.Fatalf("empty min, got %+v", min)
-		}
-		if max := tr.Max(); max != nil {
-			t.Fatalf("empty max, got %+v", max)
-		}
-		for _, item := range perm(treeSize) {
-			if x := tr.ReplaceOrInsert(item); x != nil {
-				t.Fatal("insert found item", item)
-			}
-		}
-		for _, item := range perm(treeSize) {
-			if x := tr.ReplaceOrInsert(item); x == nil {
-				t.Fatal("insert didn't find item", item)
-			}
-		}
-		if min, want := tr.Min(), Item(Int(0)); min != want {
-			t.Fatalf("min: want %+v, got %+v", want, min)
-		}
-		if max, want := tr.Max(), Item(Int(treeSize-1)); max != want {
-			t.Fatalf("max: want %+v, got %+v", want, max)
-		}
-		got := all(tr)
-		want := rang(treeSize)
-		if !reflect.DeepEqual(got, want) {
-			t.Fatalf("mismatch:\n got: %v\nwant: %v", got, want)
-		}
 
-		gotrev := allrev(tr)
-		wantrev := rangrev(treeSize)
-		if !reflect.DeepEqual(gotrev, wantrev) {
-			t.Fatalf("mismatch:\n got: %v\nwant: %v", got, want)
-		}
 
-		for _, item := range perm(treeSize) {
-			if x := tr.Delete(item); x == nil {
-				t.Fatalf("didn't find %v", item)
-			}
-		}
-		if got = all(tr); len(got) > 0 {
-			t.Fatalf("some left!: %v", got)
-		}
-	}
-}
+
+
+
 
 type byInts []Item
 
